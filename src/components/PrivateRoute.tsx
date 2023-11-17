20e5a3628b126b091ae1a5709c291a33d38bbd58
Darlyn Anderson Vailatti <darlynvailatti@gmail.com>
2023-11-17 22:36:26 +1030

    Update git actions workflow

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { Navigate, RouteProps } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 
 export function PrivateRoute({ children }: RouteProps): JSX.Element {
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
   if (loading) {
     return <div>Loading...</div>; // or your loading screen
